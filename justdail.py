bd5f64fdbce8619399c920db5dba64e94622dc08
shekolla <shekollasaikiran@gmail.com>
2023-04-27 07:39:44 +0530

    Handle new format

--- justdail.py
+++ justdail.py
@@ -3,14 +3,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--url', help='Enter the url you want to scrape')
 parser.add_argument('--file', help='Specify the file name', default="export.csv")
+parser.add_argument('--page_number', help='Specify the amount of pages to query', default=1)
+
 args = parser.parse_args()
 
 from bs4 import BeautifulSoup
 import urllib
 import urllib.request
-import requests
 import csv
-import os
 import time
 
 
@@ -18,7 +18,17 @@ def innerHTML(element):
     return element.decode_contents(formatter="html")
 
 def get_name(body):
-	return body.find('span', {'class':'jcn'}).a.attrs['title']
+	return body.find('div', {'class':'resultbox_imagebox'})
+
+def get_title(body):
+	if body is not None:
+		return name.get('title')
+
+def get_url(body):
+	if body is not None:
+		if name.a is not None:
+			return name.a.get('href')
+
 
 def which_digit(html):
     mappingDict={'icon-ji':9,
@@ -39,104 +49,78 @@ def which_digit(html):
     return mappingDict.get(html,'')
 
 def get_phone_number(body):
-    i=0
-    phoneNo = "No Number!"
-    try:
-            
-        for item in body.find('p',{'class':'contact-info'}):
-            i+=1
-            if(i==2):
-                phoneNo=''
-                try:
-                    for element in item.find_all(class_=True):
-                        classes = []
-                        classes.extend(element["class"])
-                        phoneNo+=str((which_digit(classes[1])))
-                except:
-                    pass
-    except:
-        pass
-    body = body['data-href']
-    soup = BeautifulSoup(body, 'html.parser')
-    for a in soup.find_all('a', {"id":"whatsapptriggeer"} ):
-        # print (a)
-        phoneNo = str(a['href'][-10:])
-
-
-    return phoneNo
+	phone = body.find('span',{'class':'callNowAnchor'})
+	if phone is not None:
+		return phone.text
 
 
 def get_rating(body):
-	rating = 0.0
-	text = body.find('span', {'class':'star_m'})
+	text = body.find('div', {'class':'resultbox_totalrate'})
 	if text is not None:
-		for item in text:
-			rating += float(item['class'][0][1:])/10
-
-	return rating
+		return text.text
 
 def get_rating_count(body):
-	text = body.find('span', {'class':'rt_count'}).string
-
-	# Get only digits
-	rating_count =''.join(i for i in text if i.isdigit())
-	return rating_count
+	text = body.find('div', {'class':'resultbox_countrate'})
+	if text is not None:
+		return text.text
 
 def get_address(body):
-	return body.find('span', {'class':'mrehover'}).text.strip()
+	address = body.find('div', {'class':'resultbox_address'})
+	if address is not None:
+		return address.text.strip()
 
-def get_location(body):
-	text = body.find('a', {'class':'rsmap'})
-	if text == None:
-		return
-	text_list = text['onclick'].split(",")
+# unable to find location
+# def get_location(body):
+# 	text = body.find('a', {'class':'rsmap'})
+# 	if text == None:
+# 		return
+# 	text_list = text['onclick'].split(",")
 	
-	latitutde = text_list[3].strip().replace("'", "")
-	longitude = text_list[4].strip().replace("'", "")
+# 	latitutde = text_list[3].strip().replace("'", "")
+# 	longitude = text_list[4].strip().replace("'", "")
 	
-	return latitutde + ", " + longitude
+# 	return latitutde + ", " + longitude
 
 page_number = 1
+page_limit = args.page_number
 service_count = 1
 
 
-fields = ['Name', 'Phone', 'Rating', 'Rating Count', 'Address', 'Location']
+fields = ['Title', 'Phone', 'Rating', 'Rating Count', 'Address', 'Url']
 out_file = open(args.file,'w')
 csvwriter = csv.DictWriter(out_file, delimiter=',', fieldnames=fields)
-
+csvwriter.writeheader()
 # Write fields first
 #csvwriter.writerow(dict((fn,fn) for fn in fields))
 while True:
 	time.sleep(2)
 	# Check if reached end of result
-	if page_number > 50:
+	if page_number > page_limit:
 		break
 
 	url="%s/page-%s" % (args.url, page_number)
 	print(url)
 	req = urllib.request.Request(url, headers={'User-Agent' : "Mozilla/5.0 (Windows NT 6.1; Win64; x64)"}) 
 	page = urllib.request.urlopen( req )
-	# page=urllib2.urlopen(url)
 
 	soup = BeautifulSoup(page.read(), "html.parser")
-	services = soup.find_all('li', {'class': 'cntanr'})
+	services = soup.find_all('div', {'class': 'resultbox'})
 
 	# Iterate through the 10 results in the page
 	for service_html in services:
-
 		# Parse HTML to fetch data     
 		dict_service = {}
 		name = get_name(service_html)
-		print(name);
+		title = get_title(name)
+		url = get_url(name)
 		phone = get_phone_number(service_html)
 		rating = get_rating(service_html)
 		count = get_rating_count(service_html)
 		address = get_address(service_html)
-		location = get_location(service_html)
-		if name != None:
-			dict_service['Name'] = name
+
+		if title != None:
+			dict_service['Title'] = title
 		if phone != None:
-			print('getting phone number')
 			dict_service['Phone'] = '\'' + phone
 		if rating != None:
 			dict_service['Rating'] = rating
@@ -144,8 +128,10 @@ while True:
 			dict_service['Rating Count'] = count
 		if address != None:
 			dict_service['Address'] = address
-		if location != None:
-			dict_service['Address'] = location
+		if url != None:
+			if (not url.startswith('http')):
+				url = 'https://www.justdial.com/jdmart' + url
+			dict_service['Url'] = url
 
 		# Write row to CSV
 		csvwriter.writerow(dict_service)
